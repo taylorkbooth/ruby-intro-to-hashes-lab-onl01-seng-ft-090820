caeedffa5f1aa54acdaf8f4b519ddca18d560bb0
Taylor Booth <taylorkbooth@gmail.com>
2020-09-15 19:28:11 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -3,14 +3,11 @@ def new_hash
 end
 
 def actor
- actor = {
-  :name => "Dwayne The Rock Johnson"
-   
- }
+ actor = {:name => "Dwayne The Rock Johnson"}
 end
 
 def monopoly
-	monopoly = {}
+	monopoly = {:railroads => nil}
 
 end
 
