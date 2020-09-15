29caa4de79b095810474ec83b517e0892dd57bd4
Taylor Booth <taylorkbooth@gmail.com>
2020-09-15 19:32:38 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,19 +7,16 @@ def actor
 end
 
 def monopoly
-	monopoly = {
-	  :railroads => 
-	    {:pieces => 4,
-	     :names => [],
-	     :rent_in_dollars =>[]
-	  
-	  }
-	}
+	monopoly = {:railroads => {}}
 
 end
 
 def monopoly_with_second_tier
-  monopoly = {:railroads => {}}
+  monopoly = {:railroads => {
+    :pieces => 4,
+	     :names => [],
+	     :rent_in_dollars =>[]
+  }}
   
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
 
