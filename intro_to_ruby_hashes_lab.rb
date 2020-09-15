0c190523420dd2ac249f7c994fe81334cd456894
Taylor Booth <taylorkbooth@gmail.com>
2020-09-15 19:33:22 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,10 +16,9 @@ def monopoly_with_second_tier
     :pieces => 4,
 	     :names => [],
 	     :rent_in_dollars =>[]
-  }}
+    }
+  }
   
-	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
-
 end
 
 def monopoly_with_third_tier
